4b03d60ea53d118b549c687686f6fcae7d3a7fff
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2025-01-03 21:06:27 -0500

    updated server.ts file and modified auth.ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,33 @@
-import express from 'express';
-import path from 'node:path';
-import db from './config/connection.js';
-import routes from './routes/index.js';
+import express from "express";
+import db from "./config/connection.js";
+//import Apollo
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+//import graphQL schema
+import { typeDefs, resolvers } from "../src/schemas";
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
 });
+
+const startApolloServer = async () => {
+	await server.start();
+	await db();
+
+	const PORT = process.env.PORT || 3001;
+	const app = express();
+
+	app.use(express.urlencoded({ extended: false }));
+	app.use(express.json());
+
+	app.use("/graphql", expressMiddleware(server));
+
+	app.listen(PORT, () => {
+		console.log("The server is running on ${PORT}!");
+		console.log("Use GraphQL at http://localhost:${PORT}/graphql");
+	});
+};
+
+//call async fx to start server
+startApolloServer();
--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,39 +1,46 @@
-import type { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-import dotenv from 'dotenv';
 dotenv.config();
 
+export class AuthenticationError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = "AuthenticationError";
+		Object.defineProperty(this, "name", { value: "AuthenticationError" });
+	}
+}
 interface JwtPayload {
-  _id: unknown;
-  username: string;
-  email: string,
+	_id: unknown;
+	username: string;
+	email: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authenticateToken = ({ req }: { req: any }) => {
+	//enforce token needing to be found in header
+	const authHeader = req.headers.authorization;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+	if (!authHeader) {
+		throw new AuthenticationError("Authorization header missing.");
+	}
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
+	const token = authHeader.split(" ").pop()?.trim();
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
+	if (!token) {
+		throw new AuthenticationError("Token missing from auth header.");
+	}
 
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
-  }
-};
+	//try to verify token
+	try {
+		const secretKey = process.env.JWT_SECRET_KEY || "";
+		const decoded = jwt.verify(token, secretKey, { maxAge: "2h" }) as {
+			data: JwtPayload;
+		};
+		req.user = decoded.data;
+	} catch (err) {
+		console.log("Token validation error:", err);
+		throw new AuthenticationError("Invalid or expired token.");
+	}
 
-export const signToken = (username: string, email: string, _id: unknown) => {
-  const payload = { username, email, _id };
-  const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+	return req;
 };
